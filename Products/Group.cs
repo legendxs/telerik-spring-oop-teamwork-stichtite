cdf7d2e29ca7781558a48696231227c52610187f
kolchagov@gmail.com <kolchagov@gmail.com>
2013-11-19 12:14:40 +0000

    Added products

---
+++ Products/Group.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Products
+{
+    class Group : ICloneable
+    {
+        public string GroupName { get; set; }
+
+        public Group(string groupName)
+        {
+            this.GroupName = groupName;
+        }
+
+        public override object Clone()
+        {
+            return new Group(GroupName);
+        }
+    }
+}
