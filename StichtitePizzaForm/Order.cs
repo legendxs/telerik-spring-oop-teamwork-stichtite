fa567367812a2d7c6c9e13aba83b462c6c1418be
Petar Tseperski <legendxs@gmail.com>
2013-11-24 18:07:07 +0000

    First revision of Order class.

---
+++ StichtitePizzaForm/Order.cs
@@ -0,0 +1,38 @@
+﻿namespace StichtitePizzaForm
+{
+    using System.Collections.Generic;
+
+    public class Order
+    {
+        public decimal Price { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public void AddProductToList(Product product)
+        {
+            this.Products.Add(product);
+        }
+
+        public void RemoveProductFromList(Product product)
+        {
+            this.Products.Remove(product);
+        }
+
+        public void ClearList()
+        {
+            this.Products.Clear();
+        }
+
+        public double CalculateTotalPrice()
+        {
+            double total = 0;
+
+            foreach (var product in this.Products)
+            {
+                total += product.Price;
+            }
+
+            return total;
+        }
+    }
+}
