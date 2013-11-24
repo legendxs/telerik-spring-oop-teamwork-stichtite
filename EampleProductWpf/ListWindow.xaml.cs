7e8c6bb3ada3ad4cf538758c206c473e8b302a2f
kolchagov@gmail.com <kolchagov@gmail.com>
2013-11-24 21:20:42 +0000

    Product example

---
+++ EampleProductWpf/ListWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace ExampleProductWpf
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class ListWindow : Window
+    {
+        public ListWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EampleProductWpf/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ExampleProductWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var product = new ExampleProductWpf.ProductList();
+            MainGrind.Children.Add(product.Render());
+        }
+    }
+}
