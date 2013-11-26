bf0771726e0584a318ec61bef6a28e00ff258682
erty.mitev@gmail.com <erty.mitev@gmail.com>
2013-11-26 06:14:08 +0000

    Added interface with methods for working with the CSVs as requested, will add a method for edditing a line in a CSV as soon as i decide on a good way to do it.

---
+++ StichtitePizzaForm/Interfaces/ICsvInteract.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StichtitePizzaForm.Interfaces
+{
+    interface ICsvInteract
+    {
+        string[] LineToArray(string[] holder, string filepath)
+        {
+            using(StreamReader csvReader= new StreamReader(filepath) )
+            {
+                if (csvReader.ReadLine() != null)
+                {
+                    holder = csvReader.ReadLine().Split(',');
+                }
+                else
+                {
+                    for (int i=0; i<holder.Length; i++)
+                    {
+                        holder[i] = "End of file";
+                    }
+                }
+                return holder;
+            }
+        }
+        //mind the lenght of the input array
+
+        void ArrayToLine(string[] input, string filepath, bool isAppend) //true means you append to the file, false you delete all of it first then write
+        {
+            StringBuilder lineBuilder = new StringBuilder();
+            for (int i=0; i<input.Length-1; i++)
+            {
+                lineBuilder.Append(input[i] + ",");
+            }
+            lineBuilder.Append(input[input.Length-1]);
+            using (StreamWriter writeToCsv= new StreamWriter(filepath, isAppend))
+            {
+                writeToCsv.WriteLine(lineBuilder.ToString());
+            }
+        }
+        
+    }
+}
